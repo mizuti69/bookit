8d4185feedf6fe8da2f015dd25f061b2f99365de
watch <mizuti7802@gmail.com>
2021-03-23 14:25:07 +0900

    Update docusaurus.config.js
    favicon update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ module.exports = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/logo.png',
   organizationName: 'mizuti69', // Usually your GitHub org/user name.
   projectName: 'bookit', // Usually your repo name.
   themeConfig: {
